e45e09e673a4dc5cf45ecf6124a1d021bba58979
daniyal khan <daniyal.khan@afiniti.com>
2022-06-29 17:14:28 +0500

    add lights

--- src/Experience/Sphere.js
+++ src/Experience/Sphere.js
@@ -15,7 +15,7 @@ export default class Sphere {
     if (this.debugPane) {
       this.debugFolder = this.debugPane.addFolder({
         title: 'sphere',
-        expanded: false
+        expanded: true
       });
 
       this.debugFolder.addInput(this, 'timeFrequency', {
@@ -26,26 +26,131 @@ export default class Sphere {
     }
 
     this.setGeometry();
+    this.setLights();
     this.setMaterial();
     this.setMesh();
   }
 
+  setLights() {
+    this.lights = {};
+
+    // Light A
+    this.lights.a = {};
+
+    this.lights.a.intensity = 1;
+
+    this.lights.a.color = {};
+    this.lights.a.color.value = '#ff2900';
+    this.lights.a.color.instance = new THREE.Color(this.lights.a.color.value);
+
+    this.lights.a.spherical = new THREE.Spherical(1, 0.615, 2.049);
+
+    // Light B
+    this.lights.b = {};
+
+    this.lights.b.intensity = 1;
+
+    this.lights.b.color = {};
+    this.lights.b.color.value = '#4145af';
+    this.lights.b.color.instance = new THREE.Color(this.lights.b.color.value);
+
+    this.lights.b.spherical = new THREE.Spherical(1, 2.561, -1.844);
+
+    // Debug
+    if (this.debugPane) {
+      for (const _lightName in this.lights) {
+        const light = this.lights[_lightName];
+
+        const debugFolder = this.debugFolder.addFolder({
+          title: _lightName,
+          expanded: true
+        });
+
+        debugFolder
+          .addInput(light.color, 'value', {
+            view: 'color',
+            label: 'color'
+          })
+          .on('change', () => {
+            light.color.instance.set(light.color.value);
+          });
+
+        debugFolder
+          .addInput(light, 'intensity', {
+            min: 0,
+            max: 5
+          })
+          .on('change', () => {
+            this.material.uniforms[
+              `uLight${_lightName.toUpperCase()}Intensity`
+            ].value = light.intensity;
+          });
+
+        debugFolder
+          .addInput(light.spherical, 'phi', {
+            label: 'phi',
+            min: 0,
+            max: Math.PI,
+            step: 0.001
+          })
+          .on('change', () => {
+            this.material.uniforms[
+              `uLight${_lightName.toUpperCase()}Position`
+            ].value.setFromSpherical(light.spherical);
+          });
+        debugFolder
+          .addInput(light.spherical, 'theta', {
+            label: 'theta',
+            min: -Math.PI,
+            max: Math.PI,
+            step: 0.001
+          })
+          .on('change', () => {
+            this.material.uniforms.uLightAPosition.value.setFromSpherical(
+              light.spherical
+            );
+          });
+      }
+    }
+  }
+
   setGeometry() {
     this.geometry = new THREE.SphereGeometry(1, 512, 512);
+    this.geometry.computeTangents();
+    console.log('this.geometry', this.geometry);
   }
 
   setMaterial() {
     this.material = new THREE.ShaderMaterial({
       uniforms: {
-        uTime: { value: 0 },
+        uLightAColor: { value: this.lights.a.color.instance },
+        uLightAPosition: { value: new THREE.Vector3(1.0, 1.0, 0.0) },
+        uLightAIntensity: { value: 1 },
+
+        uLightBColor: { value: this.lights.b.color.instance },
+        uLightBPosition: { value: new THREE.Vector3(-1.0, -1.0, 0.0) },
+        uLightBIntensity: { value: 1 },
+
         uDistortionFrequency: { value: 2.0 },
         uDistortionStrength: { value: 1.0 },
         uDisplacementFrequency: { value: 2.0 },
-        uDisplacementStrength: { value: 0.2 }
+        uDisplacementStrength: { value: 0.2 },
+
+        uTime: { value: 0 }
+      },
+      defines: {
+        USE_TANGENT: ''
       },
       vertexShader: vertexShader,
       fragmentShader: fragmentShader
     });
+
+    this.material.uniforms.uLightAPosition.value.setFromSpherical(
+      this.lights.a.spherical
+    );
+    this.material.uniforms.uLightBPosition.value.setFromSpherical(
+      this.lights.b.spherical
+    );
 
     if (this.debugPane) {
       this.debugFolder.addInput(
--- src/Experience/shaders/sphere/vertex.glsl
+++ src/Experience/shaders/sphere/vertex.glsl
@@ -1,3 +1,11 @@
+#define M_PI 3.1415926535897932384626433832795
+
+uniform vec3 uLightAColor;
+uniform vec3 uLightAPosition;
+uniform float uLightAIntensity;
+uniform vec3 uLightBColor;
+uniform vec3 uLightBPosition;
+uniform float uLightBIntensity;
 
 uniform float uTime;
 uniform float uDistortionFrequency;
@@ -5,24 +13,55 @@ uniform float uDistortionStrength;
 uniform float uDisplacementFrequency;
 uniform float uDisplacementStrength;
 
+varying vec3 vColor;
 varying vec3 vNormal;
 varying float vPerlinStrength;
 
 #pragma glslify: perlin4d = require('../partials/perlin4d.glsl');
 
-void main() {
-
-  vec3 displacementPosition = position;
+vec4 getDisplacedPosition(vec3 _position) {
+  vec3 displacementPosition = _position;
   displacementPosition += perlin4d(vec4(displacementPosition * uDistortionFrequency, uTime)) * uDistortionStrength;
 
   float perlinStrength = perlin4d(vec4(displacementPosition * uDisplacementFrequency, uTime));
 
-  vec3 newPosition = position;
-    newPosition += normal * perlinStrength * uDisplacementStrength;
+  vec3 displacedposition = _position;
+  displacedposition += normalize(_position) * perlinStrength * uDisplacementStrength;
 
-  vec4 viewPosition = viewMatrix * vec4(newPosition, 1.0);
+  return vec4(displacedposition, perlinStrength);
+}
+
+void main() {
+
+  // Position
+  vec4 displacedposition = getDisplacedPosition(position);
+  vec4 viewPosition = viewMatrix * vec4(displacedposition.xyz, 1.0);
   gl_Position = projectionMatrix * viewPosition;
 
+  // Bi tangent
+  float neighbourTangentDistance = (M_PI * 2.0) / 512.0;
+  float neighbourBiTangentDistance = M_PI / 512.0;
+
+  vec3 biTangent = cross(normal, tangent.xyz);
+  vec3 tangentNeighbour = position + tangent.xyz * neighbourTangentDistance;
+  tangentNeighbour = getDisplacedPosition(tangentNeighbour).xyz;
+
+  vec3 biTangentNeighbour = position + biTangent.xyz * neighbourBiTangentDistance;
+  biTangentNeighbour = getDisplacedPosition(biTangentNeighbour).xyz;
+
+  vec3 computedNormal = cross(tangentNeighbour, biTangentNeighbour);
+  computedNormal = normalize(computedNormal);
+
+  // Color - this as an old way of creating light and its super performant
+  float lightAIntensity = max(0.0, -dot(normal.xyz, normalize(- uLightAPosition))) * uLightAIntensity;
+  float lightBIntensity = max(0.0, -dot(normal.xyz, normalize(- uLightBPosition))) * uLightBIntensity;
+
+  vec3 color = vec3(0.0);
+  color = mix(color, uLightAColor, lightAIntensity);
+  color = mix(color, uLightBColor, lightBIntensity);
+
+  // Varying
   vNormal = normal;
-  vPerlinStrength = perlinStrength;
+  vPerlinStrength = displacedposition.a;
+  vColor = color;
 }
