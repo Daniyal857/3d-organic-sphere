0b93b68e97890531eabeddaede5b8edcd28a250f
daniyal khan <daniyal.khan@afiniti.com>
2022-06-30 23:37:00 +0500

    :sparkles: Add unreal bloom pass

--- src/Experience/shaders/sphere/vertex.glsl
+++ src/Experience/shaders/sphere/vertex.glsl
@@ -72,6 +72,7 @@ void main() {
   // color = mix(color, uLightAColor, fresnel);
   color = mix(color, uLightAColor, lightAIntensity * fresnel);
   color = mix(color, uLightBColor, lightBIntensity * fresnel);
+  color = mix(color, vec3(1.0), clamp(pow(fresnel - 0.8, 3.0), 0.0, 1.0));
 
   // Varying
   vNormal = normal;
