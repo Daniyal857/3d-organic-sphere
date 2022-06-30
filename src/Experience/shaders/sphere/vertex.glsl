1c74bb0a1749c0d7196eb72856e545bdb5f31e6c
daniyal khan <daniyal.khan@afiniti.com>
2022-06-30 17:29:32 +0500

    :sparkles: Add fresnel

--- src/Experience/shaders/sphere/vertex.glsl
+++ src/Experience/shaders/sphere/vertex.glsl
@@ -7,11 +7,17 @@ uniform vec3 uLightBColor;
 uniform vec3 uLightBPosition;
 uniform float uLightBIntensity;
 
+uniform vec2 uSubdivision;
+
 uniform float uTime;
 uniform float uDistortionFrequency;
 uniform float uDistortionStrength;
 uniform float uDisplacementFrequency;
 uniform float uDisplacementStrength;
+
+uniform float uFresnelOffset;
+uniform float uFresnelMultiplier;
+uniform float uFresnelPower;
 
 varying vec3 vColor;
 varying vec3 vNormal;
@@ -25,43 +31,50 @@ vec4 getDisplacedPosition(vec3 _position) {
 
   float perlinStrength = perlin4d(vec4(displacementPosition * uDisplacementFrequency, uTime));
 
-  vec3 displacedposition = _position;
-  displacedposition += normalize(_position) * perlinStrength * uDisplacementStrength;
+  vec3 displacedPosition = _position;
+  displacedPosition += normalize(_position) * perlinStrength * uDisplacementStrength;
 
-  return vec4(displacedposition, perlinStrength);
+  return vec4(displacedPosition, perlinStrength);
 }
 
 void main() {
 
   // Position
-  vec4 displacedposition = getDisplacedPosition(position);
-  vec4 viewPosition = viewMatrix * vec4(displacedposition.xyz, 1.0);
+  vec4 displacedPosition = getDisplacedPosition(position);
+  vec4 viewPosition = viewMatrix * vec4(displacedPosition.xyz, 1.0);
   gl_Position = projectionMatrix * viewPosition;
 
   // Bi tangent
-  float neighbourTangentDistance = (M_PI * 2.0) / 512.0;
-  float neighbourBiTangentDistance = M_PI / 512.0;
+  float distanceA = (M_PI * 2.0) / uSubdivision.x;
+  float distanceB = M_PI / uSubdivision.x;
 
   vec3 biTangent = cross(normal, tangent.xyz);
-  vec3 tangentNeighbour = position + tangent.xyz * neighbourTangentDistance;
-  tangentNeighbour = getDisplacedPosition(tangentNeighbour).xyz;
 
-  vec3 biTangentNeighbour = position + biTangent.xyz * neighbourBiTangentDistance;
-  biTangentNeighbour = getDisplacedPosition(biTangentNeighbour).xyz;
+  vec3 positionA = position + tangent.xyz * distanceA;
+  vec3 displacedPositionA = getDisplacedPosition(positionA).xyz;
 
-  vec3 computedNormal = cross(tangentNeighbour, biTangentNeighbour);
+  vec3 positionB = position + biTangent.xyz * distanceB;
+  vec3 displacedPositionB = getDisplacedPosition(positionB).xyz;
+
+  vec3 computedNormal = cross(displacedPositionA - displacedPosition.xyz, displacedPositionB - displacedPosition.xyz);
   computedNormal = normalize(computedNormal);
 
+  // Fresnel
+  vec3 viewDirection = normalize(displacedPosition.xyz - cameraPosition);
+  float fresnel = uFresnelOffset + (1.0 + dot(viewDirection, computedNormal)) * uFresnelMultiplier;
+  fresnel = pow(fresnel, uFresnelPower);
+
   // Color - this as an old way of creating light and its super performant
-  float lightAIntensity = max(0.0, -dot(normal.xyz, normalize(- uLightAPosition))) * uLightAIntensity;
-  float lightBIntensity = max(0.0, -dot(normal.xyz, normalize(- uLightBPosition))) * uLightBIntensity;
+  float lightAIntensity = max(0.0, -dot(computedNormal.xyz, normalize(- uLightAPosition))) * uLightAIntensity;
+  float lightBIntensity = max(0.0, -dot(computedNormal.xyz, normalize(- uLightBPosition))) * uLightBIntensity;
 
   vec3 color = vec3(0.0);
-  color = mix(color, uLightAColor, lightAIntensity);
-  color = mix(color, uLightBColor, lightBIntensity);
+  // color = mix(color, uLightAColor, fresnel);
+  color = mix(color, uLightAColor, lightAIntensity * fresnel);
+  color = mix(color, uLightBColor, lightBIntensity * fresnel);
 
   // Varying
   vNormal = normal;
-  vPerlinStrength = displacedposition.a;
+  vPerlinStrength = displacedPosition.a;
   vColor = color;
 }
