f2f698d5980eed036ea623b5061fcc964e4e4e27
daniyal khan <daniyal.khan@afiniti.com>
2022-07-02 04:16:36 +0500

    :sparkles: add random direction movement to sphere.

--- src/Experience/shaders/sphere/vertex.glsl
+++ src/Experience/shaders/sphere/vertex.glsl
@@ -9,6 +9,8 @@ uniform float uLightBIntensity;
 
 uniform vec2 uSubdivision;
 
+uniform vec3 uOffset;
+
 uniform float uTime;
 uniform float uDistortionFrequency;
 uniform float uDistortionStrength;
@@ -23,23 +25,24 @@ varying vec3 vColor;
 
 #pragma glslify: perlin4d = require('../partials/perlin4d.glsl');
 
-vec4 getDisplacedPosition(vec3 _position) {
-  vec3 displacementPosition = _position;
-  displacementPosition += perlin4d(vec4(displacementPosition * uDistortionFrequency, uTime)) * uDistortionStrength;
+vec3 getDisplacedPosition(vec3 _position) {
+  
+  vec3 distortedPosition = _position;
+  distortedPosition += perlin4d(vec4(distortedPosition * uDistortionFrequency + uOffset, uTime)) * uDistortionStrength;
 
-  float perlinStrength = perlin4d(vec4(displacementPosition * uDisplacementFrequency, uTime));
+  float perlinStrength = perlin4d(vec4(distortedPosition * uDisplacementFrequency + uOffset, uTime));
 
   vec3 displacedPosition = _position;
   displacedPosition += normalize(_position) * perlinStrength * uDisplacementStrength;
 
-  return vec4(displacedPosition, perlinStrength);
+  return displacedPosition;
 }
 
 void main() {
 
   // Position
-  vec4 displacedPosition = getDisplacedPosition(position);
-  vec4 viewPosition = viewMatrix * vec4(displacedPosition.xyz, 1.0);
+  vec3 displacedPosition = getDisplacedPosition(position);
+  vec4 viewPosition = viewMatrix * vec4(displacedPosition, 1.0);
   gl_Position = projectionMatrix * viewPosition;
 
   // Bi tangent
@@ -49,10 +52,10 @@ void main() {
   vec3 biTangent = cross(normal, tangent.xyz);
 
   vec3 positionA = position + tangent.xyz * distanceA;
-  vec3 displacedPositionA = getDisplacedPosition(positionA).xyz;
+  vec3 displacedPositionA = getDisplacedPosition(positionA);
 
   vec3 positionB = position + biTangent.xyz * distanceB;
-  vec3 displacedPositionB = getDisplacedPosition(positionB).xyz;
+  vec3 displacedPositionB = getDisplacedPosition(positionB);
 
   vec3 computedNormal = cross(displacedPositionA - displacedPosition.xyz, displacedPositionB - displacedPosition.xyz);
   computedNormal = normalize(computedNormal);
